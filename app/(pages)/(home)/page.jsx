2eb867f57a8dba4cb365270465ce1e3b6fc0c613
austin1im <alaustinall@gmail.com>
2025-07-27 00:16:22 -0700

    completion

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -3,28 +3,34 @@ import Card from "@/app/(pages)/_components/home/card.jsx"
 import styles from "./page.module.scss"
 import Image from "next/image";
 import { FaPenNib } from "react-icons/fa";
+import cards from "./cardData.json"
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.mission}>
-        <h1 className={styles.missionText}>
-          Our mission is to design creative digital solutions that have a positive social impact
-        </h1>
-        <Image width={824} height={412} src='/helloworld.png' alt='hello world' className={styles.helloImage}/>
+      <div className={styles.whiteBackground}>
+        <div className={styles.mission}>
+          <h1 className={styles.missionText}>
+            Our mission is to design creative digital solutions that have a positive social impact
+          </h1>
+          <Image width={824} height={412} src="/helloworld.png" alt="hello world" className={styles.helloImage}/>
+        </div>
       </div>
+
       <div className={styles.recent}>
-          <FaPenNib size="5rem" color="white"/>
+          <FaPenNib size="5rem" color="white" className={styles.penLogo}/>
           <h2 className={styles.recentTitle}>
             Recent Work
           </h2>
           <h3 className={styles.recentSubtitle}>
             Transforming ideas into incredible realities is what we do best.
           </h3>
-          <Card/>
-      </div>
-
-      <div className={styles.purpleBackground}>
+          <div className={styles.cardContainer}>
+            {cards.map((card, index) => (
+              <Card key={index} term={card.term} logoPath={card.logoPath} org={card.org} description={card.description} URL={card.URL}/>
+            ))}
+          </div>
+          <h4 className={styles.andMore}> AND MORE... </h4>
       </div>
     </main>
   );
--- app/(pages)/_components/home/card.jsx
+++ app/(pages)/_components/home/card.jsx
@@ -1,9 +1,24 @@
 "use client";
+import styles from "./card.module.scss"
+import Image from "next/image";
 
-export default function Card() {
+export default function Card({term, logoPath, org, description, URL}) {
     return (
-      <main>
-        hi
+      <main className={styles.card}>
+        <h4 className={styles.term}>
+          {term}
+        </h4>
+        <Image width={523} height={300} src={logoPath} alt={org} className={styles.logo}/>
+        <h3 className={styles.org}>
+          {org}
+        </h3>
+        <p className={styles.description}>
+          {description}
+        </p>
+        <a className={styles.redirectButton} href={URL} target="_blank" rel="noopener noreferrer" >
+          <div className={styles.greenCircle}></div>
+          VISIT SITE
+        </a>
       </main>
     );
   }
